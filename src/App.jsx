b49117a7eada1a059676b4c22d1a0065a01360e6
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-26 11:14:50 +0600

    create LoginPage successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
-
+import LoginPage from "./pages/LoginPage"
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route element={<HomePage></HomePage>} path="" exact></Route>
+        <Route element={<LoginPage></LoginPage>}></Route>
       </Routes>
     </>
   )
