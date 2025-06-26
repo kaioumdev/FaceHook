1d6eb812a5f1d8ec5827596dcf86bb86868c2e61
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-26 22:27:43 +0600

    complete privateRoutes successfully

---
+++ src/routes/PrivatRoutes.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth"
+
+const PrivatRoutes = () => {
+    const { auth } = useAuth();
+
+    return (
+        <>
+            {
+                auth.user ? (<main className="mx-auto max-w-[1020px] py-8">
+                    <div className="container">
+                        <Outlet></Outlet>
+                    </div>
+                </main>) : (<Navigate to="/login"></Navigate>)
+            }
+        </>
+    )
+}
+
+export default PrivatRoutes
