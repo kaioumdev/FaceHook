50befdee60cbb385405610e0fe3c47495b294a6d
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-26 21:52:44 +0600

    implements AuthProvider to the main file successfully

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -5,4 +5,4 @@ export const useAuth = () => {
     const { auth } = useContext(AuthContext);
     useDebugValue(auth, auth => auth?.user ? "User Logged In" : "User Logged Out");
     return useContext(AuthContext);
-}
+};
