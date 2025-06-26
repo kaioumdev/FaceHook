4a1ce71de368b7964c2179c020bff47ff994f9c7
kaiyumdev <afrankaiyum34@gmail.com>
2025-06-26 18:15:27 +0600

    create getChild function to get id at Field components

--- src/components/common/Field.jsx
+++ src/components/common/Field.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
-const Field = () => {
+const Field = ({ label, children, htmlFor, error }) => {
+    const id = htmlFor || getChildId(children)
     return (
-        <div>Field</div>
+        <div className='form-control'>
+            {label && <label htmlFor={id} className='auth-label'>{label}
+                {children}</label>}
+            {!!error && <div role="alert" className='text-red-600'>{error.message}</div>}
+        </div>
     )
+}
+
+const getChildId = (children) => {
+    const child = React.Children.only(children);
+    if ("id" in child?.props) {
+        return child.props.id;
+    }
 }
 
 export default Field
